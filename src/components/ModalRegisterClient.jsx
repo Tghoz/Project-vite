8d30451344fa995fceee87ecd9a5a4359da0df89
tgoz <govannytgoz@gmail.com>
2024-02-25 00:43:08 -0400

    formulario registro a cliente

--- src/components/ModalRegisterClient.jsx
+++ src/components/ModalRegisterClient.jsx
@@ -1,6 +1,5 @@
-
-import React from "react";
 import { FaUserPlus } from "react-icons/fa";
+
 import {
   Modal,
   ModalContent,
@@ -9,84 +8,108 @@ import {
   ModalFooter,
   Button,
   useDisclosure,
-  Input,
 } from "@nextui-org/react";
 
+import { Input } from "@nextui-org/react";
+
 export default function App() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [backdrop, setBackdrop] = React.useState("opaque");
-
-  const backdrops = ["blur"];
-
-  const handleOpen = (backdrop) => {
-    setBackdrop(backdrop);
-    onOpen();
-  };
-
-  const handleSubmit = () => {
-    onClose();
-  };
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
-
-
-      <div className="flex flex-wrap ">
-
-      <div>
-
-        {backdrops.map((b) => (
-          <Button
-            key={b}
-            variant="flat"
-            color="warning"
-            onPress={() => handleOpen(b)}
-
-            className="capitalize">
-            <FaUserPlus size={25} />
-
-      
-            Registrar Cliente
-          </Button>
-        ))}
-      </div>
-      <Modal backdrop={backdrop} isOpen={isOpen} onClose={onClose}>
+    <div className="">
+      <Button
+        onPress={onOpen}
+        variant="flat"
+        className="capitalize"
+        color="warning">
+        <FaUserPlus size={20} />
+        Registrar Cliente
+      </Button>
+      <Modal size={"2xl"} isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader>
-                <h2>Registro de Cliente</h2>
+              <ModalHeader className="flex flex-col gap-1">
+                Registo a Clienete
               </ModalHeader>
               <ModalBody>
-                <form>
-                  <Input label="Nombre" />
-                  <Input label="Apellido" />
-                  <Input label="Documento de Identidad" />
-                  <Input type="email" label="Correo Electrónico" />
-                  <Input label="Teléfono" />
-                  <Input label="Dirección" />
-                  <div>
-                    <label htmlFor="gender">Género</label>
-                    <select id="gender" name="gender">
-                      <option value="masculino">Masculino</option>
-                      <option value="femenino">Femenino</option>
-                      <option value="otro">Otro</option>
-                    </select>
+                <form action="" method="post" className="flex flex-col gap-4">
+                  <div className="flex gap-2">
+                    <Input type="nombre" label="Nombre" className="max-w-xs" />
+                    <Input
+                      type="apellido"
+                      label="Apellido"
+                      className="max-w-xs"
+                    />
                   </div>
-                  <Input type="date" label="Fecha de Nacimiento" />
+                  <div className="flex gap-2">
+                    <Input
+                      label="Documento de Identidad"
+                      placeholder="27797609"
+                      labelPlacement="outside"
+                      startContent={
+                        <div className="flex items-center">
+                          <label className="sr-only" htmlFor="currency">
+                            Tipo
+                          </label>
+                          <select
+                            className="outline-none border-0 bg-transparent text-default-400 text-small"
+                            id="currency"
+                            name="currency">
+                            <option>Natural</option>
+                            <option>Juridico</option>
+                            <option>Gubernametal</option>
+                          </select>
+                        </div>
+                      }
+                      type="text"
+                    />
+                    <Input
+                      label="Numero de Contacto"
+                      placeholder="2288105"
+                      labelPlacement="outside"
+                      startContent={
+                        <div className="flex items-center">
+                          <label className="sr-only" htmlFor="currency">
+                            idk
+                          </label>
+                          <select
+                            className="outline-none border-0 bg-transparent text-default-400 text-small"
+                            id="currency"
+                            name="currency">
+                            <option>0412</option>
+                            <option>0424</option>
+                            <option>0416</option>
+                          </select>
+                        </div>
+                      }
+                      type="text"
+                    />
+                  </div>
+                  <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                    <Input type="date" />
+                  </div>
+                  <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                    <Input
+                      type="texto"
+                      label="Direccion"
+                      description="No tienes que ser espesifico si no quieres"
+                    />
+                  </div>
                 </form>
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
-                  Cerrar
+                  Cancelar
                 </Button>
-                <Button color="primary" onPress={handleSubmit}>
-                  Registrar
+                <Button color="primary" onPress={onClose}>
+                  Guardar
                 </Button>
               </ModalFooter>
             </>
           )}
         </ModalContent>
       </Modal>
-    
+    </div>
   );
 }
