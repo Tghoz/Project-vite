d54d86a72a9942592a134b5fe3abc1fbb12ff2ff
Mauricio2308 <diomedesramires08@gmail.com>
2024-02-26 18:45:58 -0400

    asdasd

--- src/components/ModalRegisterClient.jsx
+++ src/components/ModalRegisterClient.jsx
@@ -1,4 +1,5 @@
 import { FaUserPlus } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 import {
   Modal,
   ModalContent,
@@ -9,46 +10,57 @@ import {
   useDisclosure,
 } from "@nextui-org/react";
 import { Input } from "@nextui-org/react";
-
 import { Select, SelectItem } from "@nextui-org/react";
 
 export default function App() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return (
-    <div className="">
-      <Button
-        onPress={onOpen}
-        variant="flat"
-        className="capitalize"
-        color="warning">
-        <FaUserPlus size={20} />
-        Registrar Cliente
-      </Button>
-      <Modal size={"2xl"} isOpen={isOpen} onClose={onClose}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">
-                Registro de Cliente
-              </ModalHeader>
-              <ModalBody>
-                <form action="" method="post" className="flex flex-col gap-4">
+  const Formulario = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+      console.log(data);
+    };
+
+    return (
+      <div className="">
+        <Button
+          onClick={onOpen}
+          variant="flat"
+          className="capitalize"
+          color="warning" >
+          <FaUserPlus size={20} />
+          Registrar Cliente
+        </Button>
+        <Modal size={"2xl"} isOpen={isOpen} onClose={onClose}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Registro de Cliente
+                </ModalHeader>
+                <ModalBody>
+                  <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    action=""
+                    method="post"
+                    className="flex flex-col gap-4" >
+                    <div className="flex gap-2">
+                      <Input type="nombre" {...register('nombre')}
+                       label="Nombre"
+                        className="max-w-xs" />
+                      <Input
+                        type="apellido" {...register('Apellido')}
+                        label="Apellido"
+                        className="max-w-xs"/>
+                      <Select className="max-w-xs"{...register('genero')} label="Genero">
+                        <SelectItem>Femenino</SelectItem>
+                        <SelectItem>Masculino</SelectItem>
+                      </Select>
+                    </div>
                   <div className="flex gap-2">
-                    <Input type="nombre" label="Nombre" className="max-w-xs" />
                     <Input
-                      type="apellido"
-                      label="Apellido"
-                      className="max-w-xs"
-                    />
-                    <Select className="max-w-xs" label="Genero">
-                      <SelectItem>Femenino</SelectItem>
-                      <SelectItem>Masculino</SelectItem>
-                    </Select>
-                  </div>
-                  <div className="flex gap-2">
-                    <Input
-                      label="Documento de Identidad"
+                      label="Documento de Identidad" {...register('Documento de identidad')}
                       placeholder="27797609"
                       labelPlacement="outside"
                       startContent={
@@ -65,57 +77,49 @@ export default function App() {
                           </select>
                         </div>
                       }
-                      type="text"
-                    />
+                      type="text" />
                     <Input
-                      label="Numero de Contacto"
-                      placeholder="2288105"
+                      label="Numero de Contacto" {...register('Numero')}
+                      placeholder="0424-2358145"
                       labelPlacement="outside"
                       startContent={
                         <div className="flex items-center">
                           <label className="sr-only" htmlFor="currency">
                             idk
                           </label>
-                          <select
-                            className="outline-none border-0 bg-transparent text-default-400 text-small"
-                            id="currency"
-                            name="currency">
-                            <option>0412</option>
-                            <option>0424</option>
-                            <option>0416</option>
-                          </select>
                         </div>
                       }
                       type="text"
                     />
                   </div>
                   <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                    <Input type="date" />
+                    <Input type="date"{...register('fecha de nacimiento')} />
                   </div>
                   <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
-                    <Input type="texto" label="Correo electrónico" />
+                    <Input type="texto" label="Correo electrónico" {...register('correo')} />
                   </div>
                   <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
                     <Input
-                      type="texto"
+                      type="texto" {...register('dirrecion')}
                       label="Dirección"
-                      description="Zona de Residencia actual"
-                    />
+                      description="Zona de Residencia actual" />
                   </div>
-                </form>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Cancelar
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Guardar
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </div>
-  );
+                  <ModalFooter>
+                      <Button color="danger" variant="light" onClick={onClose}>
+                        Cancelar
+                      </Button>
+                      <Button color="primary" type="submit">
+                        Guardar
+                      </Button>
+                    </ModalFooter>
+                  </form>
+                </ModalBody>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      </div>
+    );
+  };
+  return <Formulario />;
 }
